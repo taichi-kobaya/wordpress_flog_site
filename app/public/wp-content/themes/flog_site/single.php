600c87f8c9e23e07a9be2f0f8ad81efc1ba146c2
小林太一 <taichi_kobayashi@kobayashitaichinoiMac.local>
2021-08-08 16:00:38 +0900

    投稿ページテンプレート作成、記事作成・テスト

---
+++ app/public/wp-content/themes/flog_site/single.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <?php get_header(); ?>
+</head>
+
+<body>
+    <div id="wrapper">
+        <?php if (have_posts()) : ?>
+            <?php get_template_part('includes/header'); ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <div class="post-heading">
+                    <h1><?php the_title(); ?></h1>
+                    <p><?php the_date(); ?></p>
+                </div>
+
+                <div class="single-content">
+                    <?php global $post;
+                    echo $post->post_content;
+                    ?>
+                </div>
+
+            <?php endwhile; ?>
+        <?php endif; ?>
+
+        <?php get_template_part('includes/footer'); ?>
+
+        <?php get_footer(); ?>
+</body>
+
+</html>
