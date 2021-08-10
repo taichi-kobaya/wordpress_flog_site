2016668015a7e84b663e34eacb36e85a0ebfa663
taichi-kobaya <higoronookonaigadaiji@gmail.com>
2021-08-10 23:04:32 +0900

    記事一覧ページ作成

---
+++ app/public/wp-content/themes/flog_site/page-2.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Template Name: ずかん
+*/
+?>
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <?php get_header(); ?>
+</head>
+
+<body>
+    <div id="wrapper">
+        <?php if (have_posts()) : ?>
+            <?php get_template_part('includes/header'); ?>
+            <?php while (have_posts()) : the_post(); ?>
+
+                <?php
+                $paged = (int) get_query_var('paged');
+                $args = array(
+                    'posts_per_page' => 5, // 表示件数
+                    'paged' => $paged,
+                    'orderby' => 'post_date', // 並び順を日付順
+                    'order' => 'DESC', // 並び順を降順
+                    'post_type' => 'post', // 投稿の一覧を表示
+                    'post_status' => 'publish' // 公開済みのものを表示
+                );
+                $the_query = new WP_Query($args);
+                if ($the_query->have_posts()) :
+                    while ($the_query->have_posts()) : $the_query->the_post();
+                ?>
+                        <ul>
+                            <li class="yokonarabi">
+                                <div class="zenkiji_img">
+                                    <div class="thumbnail">
+                                        <a href="<?php echo get_permalink(); ?>">
+                                            <?php echo get_the_post_thumbnail(); ?>
+                                    </div>
+                                    <?php the_title(); ?>
+                                    <h3 class="post-subtitle">
+                                        <?php the_excerpt(); ?>
+                                    </h3>
+                                    <p><?php echo get_the_date(); ?></p>
+                                </div>
+                                </a>
+                            </li>
+                        </ul>
+                <?php endwhile;
+                endif; ?>
+                <?php wp_reset_postdata(); ?>
+
+                <div class="pagenate">
+                    <?php
+                    if ($the_query->max_num_pages > 1) {
+                        echo paginate_links(array(
+                            'base' => get_pagenum_link(1) . '%_%',
+                            'format' => 'page/%#%/',
+                            'current' => max(1, $paged),
+                            'total' => $the_query->max_num_pages
+                        ));
+                    }
+                    ?>
+                </div>
+
+            <?php endwhile; ?>
+        <?php endif; ?>
+
+        <?php get_template_part('includes/footer'); ?>
+
+        <?php get_footer(); ?>
+</body>
+
+</html>
